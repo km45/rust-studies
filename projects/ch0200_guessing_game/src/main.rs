f6cba388341f3a113db78f12c81dcd823bc96913
Kamei Shogo <km45@users.noreply.github.com>
2020-05-17 09:41:06 +0900

    read user's input and print its value

--- projects/ch0200_guessing_game/src/main.rs
+++ projects/ch0200_guessing_game/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // - variables are implicitly immutable in Rust,
+    //   so make variables explicitly mutable with `mut` to be them mutable.
+    let mut guess = String::new();
+
+    std::io::stdin()
+        // - `&` indicates "(immutable) reference" (implicitly immutable in Rust)
+        // - `&mut` indicates "mutable reference"
+        .read_line(&mut guess)
+        // - `read_line` returns `io::Result`, which is enum (Ok, Err)
+        // - `.expect` causes crash if `io::Result` equals `Err`.
+        // - compiler warn if not call `expect` (different from C++)
+        .expect("Failed to read line");
+
+    // `println!` macro can treat placeholder `{}`
+    println!("You guessed: {}", guess);
 }
