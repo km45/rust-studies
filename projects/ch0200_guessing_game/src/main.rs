44a4324b2ff9f3a0822a617df6390298f63e43d3
Kamei Shogo <km45@users.noreply.github.com>
2020-05-17 10:17:19 +0900

    loop user's trial until user win

--- projects/ch0200_guessing_game/src/main.rs
+++ projects/ch0200_guessing_game/src/main.rs
@@ -10,32 +10,37 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // - variables are implicitly immutable in Rust,
-    //   so make variables explicitly mutable with `mut` to be them mutable.
-    let mut guess = String::new();
+        // - variables are implicitly immutable in Rust,
+        //   so make variables explicitly mutable with `mut` to be them mutable.
+        let mut guess = String::new();
 
-    std::io::stdin()
-        // - `&` indicates "(immutable) reference" (implicitly immutable in Rust)
-        // - `&mut` indicates "mutable reference"
-        .read_line(&mut guess)
-        // - `read_line` returns `io::Result`, which is enum (Ok, Err)
-        // - `.expect` causes crash if `io::Result` equals `Err`.
-        // - compiler warn if not call `expect` (different from C++)
-        .expect("Failed to read line");
+        std::io::stdin()
+            // - `&` indicates "(immutable) reference" (implicitly immutable in Rust)
+            // - `&mut` indicates "mutable reference"
+            .read_line(&mut guess)
+            // - `read_line` returns `io::Result`, which is enum (Ok, Err)
+            // - `.expect` causes crash if `io::Result` equals `Err`.
+            // - compiler warn if not call `expect` (different from C++)
+            .expect("Failed to read line");
 
-    // - convert String to u32
-    // - shadow guess
-    //   - use `guess` for u32 value though already exist `guess` for String
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // - convert String to u32
+        // - shadow guess
+        //   - use `guess` for u32 value though already exist `guess` for String
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    // `println!` macro can treat placeholder `{}`
-    println!("You guessed: {}", guess);
+        // `println!` macro can treat placeholder `{}`
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small"),
-        std::cmp::Ordering::Equal => println!("You Win!"),
-        std::cmp::Ordering::Greater => println!("Too big!"),
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small"),
+            std::cmp::Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+            std::cmp::Ordering::Greater => println!("Too big!"),
+        }
     }
 }
