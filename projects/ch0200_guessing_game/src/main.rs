9795dd244c16b6045c6d8ef089660083be7ba6d6
Kamei Shogo <km45@users.noreply.github.com>
2020-05-17 09:58:33 +0900

    generate random number and print its value

--- projects/ch0200_guessing_game/src/main.rs
+++ projects/ch0200_guessing_game/src/main.rs
@@ -1,5 +1,14 @@
+// MEMO: Is this requires?
+// extern crate rand;
+
+// MEMO: Why is this required?
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
