28b0f5847ce4e40bb5ccd30e847bf669f1b9bee0
Kamei Shogo <km45@users.noreply.github.com>
2022-04-26 01:57:00 +0900

    Add tests for Config::new

--- projects/ch1299_io_project/minigrep/src/lib.rs
+++ projects/ch1299_io_project/minigrep/src/lib.rs
@@ -30,3 +30,29 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_config_fails_for_less_than_three_arguments() {
+        {
+            let args = vec![String::from("a")];
+            let result = Config::new(&args);
+            assert!(result.is_err());
+        }
+        {
+            let args = vec![String::from("a"), String::from("b")];
+            let result = Config::new(&args);
+            assert!(result.is_err());
+        }
+    }
+
+    #[test]
+    fn create_config_succeeds_for_three_arguments() {
+        let args = vec![String::from("a"), String::from("b"), String::from("c")];
+        let result = Config::new(&args);
+        assert!(result.is_ok());
+    }
+}
