dd9cef74e525dfa8c898f109f2086f6b887e8c07
Kamei Shogo <km45@users.noreply.github.com>
2022-04-26 22:26:59 +0900

    [ch1206] Print error message to stderr

--- projects/ch1299_io_project/minigrep/src/main.rs
+++ projects/ch1299_io_project/minigrep/src/main.rs
@@ -11,13 +11,13 @@ fn main() {
     let config = Config::new(&args).unwrap_or_else(
         // use "closure" explained in chapter 13
         |err| {
-            println!("Problem parsing arguments: {}", err);
+            eprintln!("Problem parsing arguments: {}", err);
             process::exit(1);
         },
     );
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
