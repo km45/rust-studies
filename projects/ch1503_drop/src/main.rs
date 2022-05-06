ba70f5560220f25b587ca0507077af75ffb985d5
Kamei Shogo <km45@users.noreply.github.com>
2022-05-07 00:36:58 +0900

    Add ch1503_drop

---
+++ projects/ch1503_drop/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    {
+        let c = CustomSmartPointer {
+            data: String::from("my stuff"),
+        };
+        let d = CustomSmartPointer {
+            data: String::from("other stuff"),
+        };
+        println!("CustomSmartPointers created.");
+    }
+    println!("===");
+    {
+        let c = CustomSmartPointer {
+            data: String::from("my stuff"),
+        };
+        drop(c);
+        let d = CustomSmartPointer {
+            data: String::from("other stuff"),
+        };
+        println!("CustomSmartPointers created.");
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
